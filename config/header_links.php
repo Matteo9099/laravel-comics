fcbef46ee19298a7d7d36907493a9070d5cb9099
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-28 15:52:27 +0200

    Push

---
+++ config/header_links.php
@@ -0,0 +1,14 @@
+<?php
+
+$links = [
+            ['text'=> "CHARACTERS", 'current' => false ],
+            ['text'=> "COMICS", 'current' => true ],
+            ['text'=> "MOVIES", 'current' => false ],
+            ['text'=> "TV", 'current' => false ],
+            ['text'=> "GAMES", 'current' => false ],
+            ['text'=> "COLLECTIBLES", 'current' => false ],
+            ['text'=> "VIDEOS", 'current' => false ],
+            ['text'=> "FANS", 'current' => false ],
+            ['text'=> "NEWS", 'current' => false ],
+            ['text'=> "SHOP", 'current' => false ],
+        ];
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+
+
+<header>
+    <div class="container">
+       <a href="#"><img src="{{asset('img/dc-logo.png')}}" alt="DC Logo"></a>
+    </div>
+</header>
