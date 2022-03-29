a31a73cab917d38a78c798b315166a76f7bbfb10
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-29 17:17:47 +0200

    Push

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -22,4 +22,46 @@
         
     </div>
 
+
+    <div class="container">
+        {{-- Talent --}}
+        <div class="talent">
+            <h3>Talent</h3>
+            <div class="details">
+                <span>Art by:</span>
+                <p>
+                    @foreach ($comic['artists'] as $artist)
+                        <a href="#">{{$artist}}</a>
+                    @endforeach
+                </p>
+                <span>Written by:</span>
+                <p>
+                    @foreach ($comic['writers'] as $writer)
+                        <a href="#">{{$writer}}</a>
+                    @endforeach
+                </p>
+            </div>
+        </div>
+
+        {{-- Specs --}}
+        <div class="specs">
+            <h3>Specs</h3>
+            <div class="details">
+                <span>Series:</span>
+                <div class="details_item">
+                    <a href="#">{{$comic['series']}}</a>
+                </div>
+                <span>U.S. Price</span>
+                <div class="details_item">
+                    <a href="#">{{$comic['price']}}</a>
+                </div>
+                <span>On Sale Date:</span>
+                <div class="details_item">
+                    <a href="#">{{$comic['sale_date']}}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
