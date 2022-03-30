95a3702c1074a7fc3bf71045d8ae33226b8939e3
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-30 18:58:55 +0200

    Commit

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -6,16 +6,24 @@
 
     <div class="jumbotron"></div>
 
-    <div class="container">
-        <div class="figurina container-fluid">
-            <img class="cover_fumetto" src="{{$comic['thumb']}}" alt="">
+    <div class="figurina">
+        <div class="container">
+            <img class="cover_fumetto" src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
         </div>
+    </div>
+    <div class="container">
         
         <div class="descrizione">
             <h3>{{$comic['title']}}</h3>
 
-            <div class="price_status">
-                <span>U.S. Price: {{$comic['price']}}</span>
+            <div class="sold">
+                <div class="price_status">
+                    <span>U.S. Price: <span class="price">{{$comic['price']}}</span></span>
+                    <span>AVAILABLE</span>
+                </div>
+                <div class="status_check">
+                    <span class="price">Check Availability</span>
+                </div>
             </div>
 
             <p>{{$comic['description']}}</p>
@@ -24,46 +32,52 @@
         
     </div>
 
-
-    <div class="container">
-        {{-- Talent --}}
-        <div class="talent">
-            <h3>Talent</h3>
-            <div class="details">
-                <span>Art by:</span>
-                <p>
-                    @foreach ($comic['artists'] as $artist)
-                        <a href="#">{{$artist}}</a>
-                    @endforeach
-                </p>
-                <span>Written by:</span>
-                <p>
-                    @foreach ($comic['writers'] as $writer)
-                        <a href="#">{{$writer}}</a>
-                    @endforeach
-                </p>
-            </div>
-        </div>
-
-        {{-- Specs --}}
-        <div class="specs">
-            <h3>Specs</h3>
-            <div class="details">
-                <span>Series:</span>
-                <div class="details_item">
-                    <a href="#">{{$comic['series']}}</a>
+    <div class="section">
+        <div class="container-group container">
+            
+            {{-- Talent --}}
+            <div class="talent">
+                <h3>Talent</h3>
+                <div class="details_art">
+                    <span>Art by:</span>
+                    <p>
+                        @foreach ($comic['artists'] as $artist)
+                            <a href="#">{{$artist}}</a>
+                        @endforeach
+                    </p>
                 </div>
-                <span>U.S. Price</span>
-                <div class="details_item">
+                <div class="details_art">
+                    <span>Written by:</span>
+                    <p>
+                        @foreach ($comic['writers'] as $writer)
+                            <a href="#">{{$writer}}</a>
+                        @endforeach
+                    </p>
+                </div>
+            </div>
+    
+            {{-- Specs --}}
+            <div class="specs">
+                <h3>Specs</h3>
+                <div class="details_series">
+                    <span>Series:</span>
+                    <div class="details_item">
+                        <a href="#">{{$comic['series']}}</a>
+                    </div>
+                </div>
+                <div class="details_series">
+                    <span>U.S. Price:</span>
                     <a href="#">{{$comic['price']}}</a>
                 </div>
-                <span>On Sale Date:</span>
-                <div class="details_item">
-                    <a href="#">{{$comic['sale_date']}}</a>
+                <div class="details_series">
+                    <span>On Sale Date:</span>
+                    <div class="details_item">
+                        <a href="#">{{$comic['sale_date']}}</a>
+                    </div>
                 </div>
             </div>
+
         </div>
-    </div>
 
 
 @endsection
